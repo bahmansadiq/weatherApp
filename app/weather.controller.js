3256cabd324a5de56aeb325b5e444b31e00c678f
bahmansadiq <bahman.sadiq@gmail.com>
2016-10-07 13:40:16 -0700

    Build weather app toastr working

--- app/weather.controller.js
+++ app/weather.controller.js
@@ -5,10 +5,10 @@
         .module('WeatherApp')
         .controller('WeatherController', WeatherController);
 
-    WeatherController.$inject = ['$http', 'weatherFactory'];
+    WeatherController.$inject = ['$http', 'weatherFactory', 'toastr'];
 
     /* @ngInject */
-    function WeatherController($http, weatherFactory) {
+    function WeatherController($http, weatherFactory, toastr) {
         var vm = this;
         vm.searchPlace;
         vm.name;
@@ -39,13 +39,13 @@
         	weatherFactory.getWeather(searchPlace).
         	then(function(result){
 				vm.name=result.data.name;
-				vm.humidity=result.data.main.humidity;
+				vm.humidity=(result.data.main.humidity);
 				vm.id=result.data.id;
-				vm.maxTemp= result.data.main.temp_max;
-				vm.minTemp= result.data.main.temp_min;
+				vm.maxTemp= Math.ceil((result.data.main.temp_max -273.15)* 1.8000   +32);
+				vm.minTemp= Math.floor((result.data.main.temp_min -273.15)* 1.8000   +32);
+				vm.temp= Math.trunc((result.data.main.temp -273.15)* 1.8000   +32);
 				vm.pressure=result.data.main.pressure;
-				vm.temp= result.data.main.temp;
-				vm.windSpeed=result.data.wind.speed;
+				vm.windSpeed=result.data.wind.speed * 10;
 				vm.dt=  Date(result.data.dt * 1000).replace('GMT-0700 (Pacific Daylight Time)' ,"");
 				vm.lat= result.data.coord.lat;
 				vm.lon= result.data.coord.lon;
@@ -56,10 +56,15 @@
 						name: vm.name,
 						timeAndDate: vm.dt
 						});
+				toastr.success("Every thing is working!")
+			},
+			function(error){
+				toastr.error("not working  " + error.data.name);
+			}
 
-        	});
 
-        };
+        );
 
     };
+};
 })();
